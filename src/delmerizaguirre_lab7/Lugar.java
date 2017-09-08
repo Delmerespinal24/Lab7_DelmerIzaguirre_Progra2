f3c31b58143c4fcf43d0227ed6a140756d2e3118
Delmer Espinal <Delmerespinal2@gmail.com>
2017-09-08 14:00:26 -0600

    Agregando clases

--- src/delmerizaguirre_lab7/Lugar.java
+++ src/delmerizaguirre_lab7/Lugar.java
@@ -2,5 +2,71 @@
 package delmerizaguirre_lab7;
 
 public class Lugar {
+       private String nombre;
+       private String clima;
+       private double extension;
+       private int habitantes;
+       private String tipoZona;
+
+    public Lugar() {
+    }
+ 
+    public Lugar(String nombre, String clima, double extension, int habitantes, String tipoZona) {
+        this.nombre = nombre;
+        this.clima = clima;
+        this.extension = extension;
+        this.habitantes = habitantes;
+        this.tipoZona = tipoZona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getClima() {
+        return clima;
+    }
+
+    public void setClima(String clima) {
+        this.clima = clima;
+    }
+
+    public double getExtension() {
+        return extension;
+    }
+
+    public void setExtension(double extension) {
+        this.extension = extension;
+    }
+
+    public int getHabitantes() {
+        return habitantes;
+    }
+
+    public void setHabitantes(int habitantes) {
+        this.habitantes = habitantes;
+    }
+
+    public String getTipoZona() {
+        return tipoZona;
+    }
+
+    public void setTipoZona(String tipoZona) {
+        this.tipoZona = tipoZona;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    
+    
+       
+       
+    
     
 }
---
+++ src/delmerizaguirre_lab7/Persona.java
@@ -0,0 +1,78 @@
+
+package delmerizaguirre_lab7;
+
+public class Persona {
+    private String nombre;
+    private String profesion;
+    private Lugar lugar;
+    private int ID;
+    private int edad;
+    private float estatura;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String profesion, Lugar lugar, int ID, int edad, float estatura) {
+        this.nombre = nombre;
+        this.profesion = profesion;
+        this.lugar = lugar;
+        this.ID = ID;
+        this.edad = edad;
+        this.estatura = estatura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
+    public Lugar getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(Lugar lugar) {
+        this.lugar = lugar;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public float getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(float estatura) {
+        this.estatura = estatura;
+    }
+
+    @Override
+    public String toString() {
+        return  nombre ;
+    }
+    
+    
+}
